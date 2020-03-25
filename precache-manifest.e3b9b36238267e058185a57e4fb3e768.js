db4fa6a752f661226d0c0a7e98e2cbfd566bcb9c
KarthikMydur <30320307+KarthikMydur@users.noreply.github.com>
2020-03-26 02:38:24 +0530

    Updates

---
+++ precache-manifest.e3b9b36238267e058185a57e4fb3e768.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d5b4a27cc5bf137466678c07ccdd8fd",
+    "url": "/Pixabay_image_finder/index.html"
+  },
+  {
+    "revision": "74ef33c3474fddd8b9b7",
+    "url": "/Pixabay_image_finder/static/js/2.b581570a.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/Pixabay_image_finder/static/js/2.b581570a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ffe06d5f7e18d49c6aa",
+    "url": "/Pixabay_image_finder/static/js/main.17a83511.chunk.js"
+  },
+  {
+    "revision": "2a1d35a781aa09c0f06a",
+    "url": "/Pixabay_image_finder/static/js/runtime-main.2469a865.js"
+  }
+]);
